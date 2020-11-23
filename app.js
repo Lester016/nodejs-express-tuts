c8e86a9a8edf16bc71b05faf78addfb25bef4fa2
Lester016 <lesterdeguzman1999@gmail.com>
2020-11-23 20:02:28 +0800

    mongoose

--- app.js
+++ app.js
@@ -1,59 +1,62 @@
 const express = require("express");
 const morgan = require("morgan");
 const mongoose = require("mongoose");
-const { result } = require("lodash");
+const Blog = require("./models/blog");
 
 // express app.
 const app = express();
 
 // connect to mongoDB
 const dbURI =
-  "mongodb+srv://finance-app:3u16kMsJoSGGvuhs@cluster0.zxtf6.mongodb.net/finance-app?retryWrites=true&w=majority";
+  "mongodb://finance-app:vSKSsQqDNDcpxfBT@cluster0-shard-00-00.zxtf6.mongodb.net:27017,cluster0-shard-00-01.zxtf6.mongodb.net:27017,cluster0-shard-00-02.zxtf6.mongodb.net:27017/finance-app?ssl=true&replicaSet=atlas-8gcmuw-shard-0&authSource=admin&retryWrites=true&w=majority";
 mongoose
   .connect(dbURI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then((result) => console.log("connected to db"))
+  .then((result) => app.listen(3000))
   .catch((err) => console.log(err));
 
-const Cat = mongoose.model("Cat", { name: String });
-
-const kitty = new Cat({ name: "Zildjian" });
-kitty.save().then(() => console.log("meow"));
 // register view engine.
 // app.set() let us configure the app settings.
 app.set("view engine", "ejs"); // ejs look for the view dir by default.
-
-// listens for request.
-app.listen(3000);
 
 // middleware & static files.
 app.use(express.static("static"));
 app.use(morgan("dev"));
 
-// app.use((req, res, next) => {
-//   console.log("new request made");
-//   console.log("host:", req.hostname);
-//   console.log("path:", req.path);
-//   console.log("method:", req.method);
-//   next();
-// });
+// mongoose and mongo sandbox routes
+app.get("/add-blog", (req, res) => {
+  const blog = new Blog({
+    title: "hello wlrodl",
+    snippet: "123123",
+    body: "32132131",
+  });
+  blog
+    .save()
+    .then((result) => {
+      res.send(result);
+    })
+    .catch((err) => console.log(err));
+});
+
+app.get("/single-blog", (req, res) => {
+  Blog.findById("5fbba05fbc039b82a5e40063")
+    .then((result) => res.send(result))
+    .catch((err) => console.log(err));
+});
 
 app.get("/", (req, res) => {
-  // res.send("Hello WORld");
+  Blog.find()
+    .sort({ createdAt: -1 })
+    .then((result) => {
+      // // The second argument will make the directory of the file related to the project's directory
+      // res.sendFile("./views/index.html", { root: __dirname });
 
-  // // The second argument will make the directory of the file related to the project's directory
-  // res.sendFile("./views/index.html", { root: __dirname });
-
-  const blogs = [
-    { title: "blog 1", snippet: "lorem ipsum" },
-    { title: "blog 2", snippet: "lorem ipsum" },
-    { title: "blog 3", snippet: "lorem ipsum" },
-  ];
-
-  // render ejs view.
-  res.render("index", { title: "home", blogs });
+      // render ejs view.
+      res.render("index", { title: "home", blogs: result });
+    })
+    .catch((err) => console.log(err));
 });
 
 app.get("/about", (req, res) => {
